d8e771de4301e4268a3403a06b4321a451f51430
Miguh Ruiz <rumomiguel@hotmail.com>
2015-03-30 17:06:58 +0200

    Minimal changes

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-var proyect = document.getElementsByClassName("proyect");
+// Miguh's Site v5 JS File
+
+// Variables
+var proyect1 = document.getElementById("proyect1");
+var proyect2 = document.getElementById("proyect2");
+var proyect3 = document.getElementById("proyect3");
 var proyect_square = document.getElementsByClassName("proyect_info");
+var square1 = proyect_square[0];
+var square2 = proyect_square[1];
+var square3 = proyect_square[2];
 
-function see(){
-	$( proyect_square ).removeClass( "disabled" );
+// Functions
+function see1(){
+	$( square1 ).removeClass( "disabled" );
 }
 
-function dontsee () {
-	$( proyect_square ).addClass( "disabled" );
+function dontsee1 () {
+	$( square1 ).addClass( "disabled" );
 }
 
-$( proyect ).mouseenter( see ).mouseleave( dontsee );
+function see2(){
+	$( square2 ).removeClass( "disabled" );
+}
+
+function dontsee2 () {
+	$( square2 ).addClass( "disabled" );
+}
+
+function see3(){
+	$( square3 ).removeClass( "disabled" );
+}
+
+function dontsee3 () {
+	$( square3 ).addClass( "disabled" );
+}
+
+// Instructions
+$( proyect1 ).mouseenter( see1 ).mouseleave( dontsee1 );
+$( proyect2 ).mouseenter( see2 ).mouseleave( dontsee2 );
+$( proyect3 ).mouseenter( see3 ).mouseleave( dontsee3 );
